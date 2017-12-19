e8aa90726914cb3bace9ddedaee825750e9a49c0
jackhowa <jackhowaa@gmail.com>
2017-12-18 19:41:27 -0600

    Actually can use spread and concat with the es2015 array

--- sorted-union.js
+++ sorted-union.js
@@ -1,4 +1,4 @@
 function uniteUnique(arr) {
-    return Array.from(arguments); // es2015 this is simpler than spread 
+    return [].concat(...arguments);
 }
 console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
