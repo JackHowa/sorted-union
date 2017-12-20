4292eb10d7a4b550312261a49f94e8c43eed4496
jackhowa <jackhowaa@gmail.com>
2017-12-19 19:58:17 -0600

    Use a set to ensure only uniques

--- sorted-union.js
+++ sorted-union.js
@@ -1,4 +1,12 @@
 function uniteUnique(arr) {
-    return [].concat(...arguments);
+    // concat brings the arrays together as the arguments are set in an array
+    const flatArray = [].concat(...arguments);
+
+    // set inside of spread operator doesn't allows duplicates
+    // the Set object lets you store unique values of any type, whether primitive values or object references.
+    const dedupedArray = [...new Set(flatArray)];
+
+    // sort causes numerical order
+    return dedupedArray.sort();
 }
 console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
